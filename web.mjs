24e0d248cc3df3af1bdb397d8ef94dfa28c18628
Fithi Teklom <fthiteklom@gmail.com>
2026-06-16 17:01:08 +0100

    Add current month and weekday headers

--- web.mjs
+++ web.mjs
@@ -19,35 +19,48 @@ const months = [
   "November",
   "December",
 ];
+function renderCalendar(month, year) {
+  const calendar = document.querySelector("#calendar");
+  const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const firstDay = new Date(year, month, 1);
+  console.log(firstDay);
+console.log(firstDay.getDay());
+  const daysInMonth =
+  new Date(year, month + 1, 0).getDate();
+  console.log(daysInMonth);
+
+
+  calendar.innerHTML = `<h2>${months[month]} ${year}</h2>`;
+  const headerRow = document.createElement("div");
+  weekDays.forEach((day) => {
+    const dayCell = document.createElement("span");
+    dayCell.textContent = day;
+    dayCell.style.marginRight = "18px";
+    headerRow.appendChild(dayCell);
+  });
+  calendar.appendChild(headerRow);
+}
 
 window.onload = () => {
   const monthSelect = document.querySelector("#month-select");
-
   const yearSelect = document.querySelector("#year-select");
 
   months.forEach((month, index) => {
     const option = document.createElement("option");
-
     option.value = index;
     option.textContent = month;
-
     monthSelect.appendChild(option);
   });
 
   for (let year = 1900; year <= 2100; year++) {
     const option = document.createElement("option");
-
     option.value = year;
     option.textContent = year;
-
     yearSelect.appendChild(option);
   }
 
   const today = new Date();
-
+  renderCalendar(today.getMonth(), today.getFullYear());
   monthSelect.value = today.getMonth();
-
   yearSelect.value = today.getFullYear();
-
-  console.log(daysData);
 };
